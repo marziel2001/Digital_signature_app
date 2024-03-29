1acd1564fafae4ba33479ae0ef7357fc6ac30f78
Marcel <marziel2001@gmail.com>
2024-03-29 20:48:15 +0100

    Checking signature implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hashlib
+import sys
 
 import rsa
 from Cryptodome.Cipher import AES
@@ -7,11 +8,12 @@ from Cryptodome.Random import get_random_bytes
 from hashlib import sha256
 
 
+
 def gen_and_save_keys():
     print("please provide the drive letter")
     drive = input()
 
-    (pub_key, priv_key) = rsa.newkeys(256) # 4096 will be used to sign the hash
+    (pub_key, priv_key) = rsa.newkeys(2048) # 4096 will be used to sign the hash
     #print(f"pub key {pub_key}, priv key {priv_key}")
 
     with open('pub.pem', 'w') as pubKeyFile:
@@ -68,12 +70,28 @@ def decipher_w_aes(encrypted_content, PIN):
     return unpadded_content
 
 
-def encrypt_with_rsa():
-    pass
+def encrypt_with_rsa(hash, key):
+    signature = rsa.encrypt(hash, key)
+    with open('signature.sha256.rsa', 'wb') as f:
+        f.write(signature)
 
 
-def hash_file():
-    file = "./requirements.txt"
+    print("len of signature: " + str(len(signature)))
+    return signature
+
+
+def verify_signature(file="./signature.sha256.rsa"):
+    loaded_pub_key, loaded_privkey = load_keys()
+    hash = hash_file()
+    signature = encrypt_with_rsa(hash, loaded_pub_key)
+    with open(file, "rb") as f:
+        sig_to_check = f.read()
+
+    if signature == sig_to_check:
+        print("sygnatury się zgadzają!\n")
+
+
+def hash_file(file="./requirements.txt"):
     BLOCK_SIZE = 65536
 
     hash = hashlib.sha256()
@@ -83,7 +101,8 @@ def hash_file():
             hash.update(fb)
             fb = f.read(BLOCK_SIZE)
 
-    print(hash.hexdigest())
+    print("hash: " + hash.hexdigest())
+    return hash.digest()
 
 print("Enter mode:\n" +
       "1. Generating RSA keys\n" +
@@ -94,13 +113,17 @@ mode = input()
 
 if mode == '1':
     gen_and_save_keys()
-
 if mode == '2':
     loaded_pub_key, loaded_privkey = load_keys()
     print(f"loaded pub key {loaded_pub_key}, loaded priv key {loaded_privkey}")
-
 if mode == '3':
     hash_file()
+if mode == '4':
+    _, loaded_privkey = load_keys()
+    hash = hash_file()
+    encrypt_with_rsa(hash, loaded_privkey)
+if mode == '5':
+    verify_signature()
 
 
 
